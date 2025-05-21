2abdc3164948b1ef7b0e29879836b0aa7e211207
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-05-21 10:06:33 +0200

    Creada view de Registro de Usuario

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializer import RegistroSerializer
 
-# Create your views here.
+class RegistroAPIView(APIView):
+    # Cualquier usuario puede registrarse, sin necesidad de token
+    permission_classes = []
+    authentication_classes = []
+    
+    def post (self, request):
+        serializer = RegistroSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # Se crea el token del usuario correspondiente y se guarda
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "user": RegistroSerializer(user).data,
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
