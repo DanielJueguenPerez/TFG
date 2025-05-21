8010c1919f12b2809cb398c0328b5fd995f96cdd
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-05-21 19:17:10 +0200

    Creada url de Ver datos de perfil

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import RegistroAPIView, LoginAPIView, LogoutAPIView
-
-router = routers.DefaultRouter()
+from .views import RegistroAPIView, LoginAPIView, LogoutAPIView, VerPerfilAPIView
 
 urlpatterns = [
     path('api/registro/', RegistroAPIView.as_view(), name='api-registro'),
     path('api/login/', LoginAPIView.as_view(), name='api-login'),
     path('api/logout/', LogoutAPIView.as_view(), name='api-logout'),
+    path('api/ver-perfil/', VerPerfilAPIView.as_view(), name='api-ver-perfil'),
 ]
