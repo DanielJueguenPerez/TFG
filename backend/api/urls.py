578fc36f6caf7553437dab3ec97ece2b03e345bb
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-05-21 10:12:43 +0200

    Creadas urls de Registro de Usuario

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from api import views
+from .views import RegistroAPIView
 
 router = routers.DefaultRouter()
 
+urlpattens = [
+    path('api/register/', RegistroAPIView.as_view(), name='api-registro'),
+]
