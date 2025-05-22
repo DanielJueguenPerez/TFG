2d7b973b4f930532890710e6244040e5f04e87a6
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-05-22 15:06:19 +0200

    Creado serializer de Editar datos de perfil

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -7,8 +7,8 @@ User = get_user_model()
 
 class RegistroSerializer(serializers.ModelSerializer):
     # Variables para realizar la confirmación de la contraseña
-    password = serializers.CharField (write_only=True, min_length=8)
-    password2 = serializers.CharField (write_only=True, label="Confirma tu contraseña")
+    password = serializers.CharField(write_only=True, min_length=8)
+    password2 = serializers.CharField(write_only=True, label="Confirma tu contraseña")
     
     # Campos que se van a recibir del frontend
     class Meta:
@@ -54,3 +54,42 @@ class VerPerfilSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','nombre','apellidos','email','DNI']
+        
+class EditarPerfilSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    
+    class Meta:
+        model = User
+        fields = ('username','nombre','apellidos','email','DNI','password')
+        
+    # Metodo para validar que, por cualquier circunstancia, no lleguen campos
+    # desconocidos al serializer    
+    def to_internal_value(self,data):
+        # Comparamos los datos recibidos con los campos del serializer
+        campos_desconocidos = set(data.keys()) - set(self.fields.keys())
+        # Si hay campos desconocidos, se lanza un error indicando cual
+        if campos_desconocidos:
+            raise serializers.ValidationError(
+                {campo: f"El campo '{campo}' no es válido." for campo in campos_desconocidos}
+            )
+        # Se llama al validador original para que valide los campos
+        return super().to_internal_value(data)
+    
+    # Parametros: 
+    # Instance - referencia al objeto del model que se va a actualizar
+    # validated_data - datos validados que se van a actualizar 
+    def update(self, instance, validated_data):
+        # Quitamos el password para actualizarlo con set_password, que
+        # hashea el password automaticamente
+        password = validated_data.pop('password', None)
+        
+        # Con un bucle for recorremos los datos validados y los asignamos
+        # para actualizarlos
+        for variable, valor in validated_data.items():
+            setattr(instance, variable, valor)
+            
+        if password:
+            instance.set_password(password)
+            
+        instance.save()
+        return instance
