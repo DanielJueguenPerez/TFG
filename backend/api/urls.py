14daa86567bea66813da6beca799b01c45f6990e
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-05-26 20:05:36 +0200

    Creada url de Ver favoritos

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/usuario/ver-perfil/', VerPerfilAPIView.as_view(), name='api-ver-perfil'),
     path('api/usuario/editar-perfil/', EditarPerfilAPIView.as_view(), name='api-editar-perfil'),
     path('api/grados/', VerGradosListAPIView.as_view(), name='api-ver-grados'),
-    path('api/grados/<int:id_grado>/', DestallesGradoRetrieveAPIView.as_view(), name='api-detalles-grado'),
+    path('api/grados/<int:id_grado>/', DetallesGradoRetrieveAPIView.as_view(), name='api-detalles-grado'),
     path('api/asignaturas/', BuscarAsignaturasListAPIView.as_view(), name='api-buscar-asignaturas'),
     path('api/asignaturas/<int:id_asignatura>/', DestallesAsignaturaRetrieveAPIView.as_view(), name='api-detalles-asignatura'),
     path('api/comentarios/<int:id_asignatura>/', VerComentariosAsignaturaListAPIView.as_view(), name='api-listar-comentarios'),
@@ -17,4 +17,5 @@ urlpatterns = [
     path('api/comentarios/eliminar/<int:id_comentario>/', EliminarComentarioDestroyAPIView.as_view(), name='api-eliminar-comentario'),
     path('api/favoritos/agregar/<int:id_asignatura>/', AgregarFavoritoCreateAPIView.as_view(), name='api-agregar-favorito'),
     path('api/favoritos/eliminar/<int:id_favorito>/', EliminarFavoritoDestroyAPIView.as_view(), name='api-eliminar-favorito'),
+    path('api/favoritos/lista/<int:id_usuario>/', VerFavoritosListAPIView.as_view(), name='api-ver-favoritos'),
 ]
