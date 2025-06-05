ebf14a05af603e63fa95a0630c865c215201c6ca
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-06-05 09:13:38 +0200

    Añadidos test unitarios de Busqueda de asignaturas

---
+++ backend/api/tests/test_unitarios_asignatura_busqueda.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from ..models import Grado, Asignatura
+from ..serializers import *
+
+class BuscarAsignaturasSerializerTests(TestCase):
+    def setUp(self):
+        self.grado = Grado.objects.create(
+            nombre='Grado en Ingeniería Informática',
+            url='ingenieríainformatica.com'
+        )
+        self.asignatura = Asignatura.objects.create(
+            nombre='Cálculo',
+            curso=1,
+            id_grado=self.grado
+        )
+    
+    def test_buscar_asignaturas_serializer_valido(self):
+        
+        serializer = BuscarAsignaturasSerializer(self.asignatura)
+        datos = serializer.data
+        
+        self.assertEqual(datos['id_asignatura'], self.asignatura.id_asignatura)
+        self.assertEqual(datos['nombre'], 'Cálculo')
+        self.assertEqual(datos['curso'], 1)
+        self.assertEqual(datos['id_grado'], self.asignatura.id_grado.id_grado)
+        
+    def test_buscar_asignaturas_serializer_no_campos_extra(self):
+        
+        serializer = BuscarAsignaturasSerializer(self.asignatura)
+        datos = serializer.data
+        
+        self.assertEqual(set(datos.keys()), {'id_asignatura','nombre','curso', 'id_grado'})
