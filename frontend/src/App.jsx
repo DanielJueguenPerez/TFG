3d2f4cf46fd9a518d5857d77268836981b9fa043
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-06-19 11:14:27 +0200

    Añadida ruta para ver grados en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import RegistroPage from './pages/RegistroPage';
 import LoginPage from './pages/LoginPage';
 import VerPerfilPage from './pages/VerPerfilPage';
 import EditarPerfilPage from './pages/EditarPerfilPage';
+import VerGradosPage from './pages/VerGradosPage';
 
 function App() {
   return(
@@ -14,7 +15,8 @@ function App() {
         <Route path='/usuario/registro' element={<Layout><RegistroPage /></Layout>} />
         <Route path='/usuario/login' element={<Layout><LoginPage /></Layout>} />
         <Route path='/usuario/ver-perfil' element={<Layout><VerPerfilPage /></Layout>} />
-        <Route path='usuario/editar-perfil' element={<Layout><EditarPerfilPage /></Layout>} />
+        <Route path='/usuario/editar-perfil' element={<Layout><EditarPerfilPage /></Layout>} />
+        <Route path='/grados' element={<Layout><VerGradosPage /></Layout>} />
       </Routes>
     </Router>
   );
