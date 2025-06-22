ed41514804d8c07285e8104e9c81df0d10a7ca50
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-06-22 15:11:43 +0200

    Añadido botón de volver atrás en Ver Grados

--- frontend/src/pages/VerGradosPage.jsx
+++ frontend/src/pages/VerGradosPage.jsx
@@ -1,23 +1,33 @@
-import { verGrados } from '../api/grados';
-import ListaPaginada from '../components/ListaPaginada';
-import { Link } from 'react-router-dom';
+import { verGrados } from "../api/grados";
+import ListaPaginada from "../components/ListaPaginada";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function VerGradosPage() {
-    const renderGrado = (grado) => (
-        <li key={grado.id_grado} className="p-4 border-b">
-            <Link to={`/grados/${grado.id_grado}`} className="text-blue-600 hover:underline">
-                {grado.nombre}
-            </Link>
-        </li>
-    );
+  const navigate = useNavigate();
 
-    return (
-        <div className="max-w-2xl mx-auto mt-10 px-2">
-            <h2 className=" text-2xl font-bold text-center mb-6">Grados disponibles</h2>
-            <ListaPaginada
-                recuperarDatos={verGrados}
-                renderItem={renderGrado}
-            />
-        </div>
-    );
+  const renderGrado = (grado) => (
+    <li key={grado.id_grado} className="p-4 border-b">
+      <Link
+        to={`/grados/${grado.id_grado}`}
+        className="text-blue-600 hover:underline"
+      >
+        {grado.nombre}
+      </Link>
+    </li>
+  );
+
+  return (
+    <div className="max-w-2xl mx-auto mt-10 px-2">
+      <button
+        onClick={() => navigate(-1)}
+        className="mb-4 text-sm text-blue-600"
+      >
+        ← Volver atrás
+      </button>
+      <h2 className=" text-2xl font-bold text-center mb-6">
+        Grados disponibles
+      </h2>
+      <ListaPaginada recuperarDatos={verGrados} renderItem={renderGrado} />
+    </div>
+  );
 }
