2d18caabe807c2037f5c12290da1fad7ab9f109d
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-06-22 15:08:27 +0200

    Creada página para Ver detalles de Asignatura

---
+++ frontend/src/pages/VerDetallesAsignaturaPage.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { verDetallesAsignatura } from "../api/asignaturas";
+import EstadisticasGrafica from "../components/EstadisticasGrafica";
+
+export default function VerDetallesAsignaturaPage() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [asignatura, setAsignatura] = useState(null);
+
+  useEffect(() => {
+    const recuperarAsignatura = async () => {
+      try {
+        const data = await verDetallesAsignatura(id);
+        setAsignatura(data);
+      } catch (error) {
+        console.error("Error al cargar la asignatura.");
+      }
+    };
+    recuperarAsignatura();
+  }, [id]);
+
+  if (!asignatura)
+    return <p className="text-center mt-10">Cargando asignatura...</p>;
+
+  return (
+    <div className="max-w-4xl mx-auto mt-10 px-4">
+      <button
+        onClick={() => navigate(-1)}
+        className="mb-4 text-sm text-blue-600"
+      >
+        ← Volver atrás
+      </button>
+      <h2 className="text-3xl font-bold text-center mb-2">
+        {asignatura.nombre}
+      </h2>
+      <p className="text-center text-gray-600 mb-6">
+        {asignatura.nombre_grado}
+      </p>
+      <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-6">
+        {asignatura.estadisticas_anios.map((anio) => (
+          <EstadisticasGrafica
+            key={anio["Año Academico"]}
+            año={anio["Año Academico"]}
+            datos={anio.estadisticas[0]}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
