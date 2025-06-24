4d6237ba7df00a7794cd995f6b5946042fb206ec
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-06-24 13:31:10 +0200

    Modificado el context de Usuario para manejar el usuario completo, y así poder tener acceso al id de usuario y a todos los parametros del usuario

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,38 +1,49 @@
-import { createContext, useContext, useState} from 'react';
-import { logoutUsuario } from '../api/auth';
+import { createContext, useContext, useState } from "react";
+import { logoutUsuario } from "../api/auth";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [token, setToken] = useState(localStorage.getItem('token') || null);
-    const [nombreUsuario, setNombreUsuario] = useState(localStorage.getItem('nombreUsuario') || null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [user, setUser] = useState(
+    localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null
+  )
+  const [nombreUsuario, setNombreUsuario] = useState(
+    localStorage.getItem("nombreUsuario") || null
+  );
 
-    const estaLogueado = !!token;
+  const estaLogueado = !!token;
 
-    const login = (newToken, nombre) => {
-        localStorage.setItem('token', newToken);
-        localStorage.setItem('nombreUsuario', nombre);
-        setToken(newToken);
-        setNombreUsuario(nombre);
-    };
+  const login = (newToken, userInfo) => {
+    localStorage.setItem("token", newToken);
+    localStorage.setItem("user", JSON.stringify(userInfo))
+    localStorage.setItem("nombreUsuario", userInfo.username);
+    setToken(newToken);
+    setUser(userInfo);
+    setNombreUsuario(userInfo.username);
+  };
 
-    const logout = async () => {
-        try {
-            await logoutUsuario();
-        } catch (error) {
-            console.error('Error al cerrar sesión:', error);
-        }
-        localStorage.removeItem('token');
-        localStorage.removeItem('nombreUsuario');
-        setToken(null);
-        setNombreUsuario(null);
-    };
+  const logout = async () => {
+    try {
+      await logoutUsuario();
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("nombreUsuario");
+    setToken(null);
+    setUser(null);
+    setNombreUsuario(null);
+  };
 
-    return(
-        <UserContext.Provider value={{token, nombreUsuario, login, logout, estaLogueado}}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider
+      value={{ token, user, nombreUsuario, login, logout, estaLogueado }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUser = () => useContext(UserContext);
