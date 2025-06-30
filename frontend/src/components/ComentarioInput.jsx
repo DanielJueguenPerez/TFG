8d45c3480d2a1a63f21532da26b31e5f570be4fc
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-06-30 09:16:57 +0200

    Corregido solapamiento de botones en editar comentario

--- frontend/src/components/ComentarioInput.jsx
+++ frontend/src/components/ComentarioInput.jsx
@@ -25,9 +25,9 @@ export default function ComentarioInput({
   };
 
   const baseBtn = `inline-flex items-center justify-center
-    text-white font-medium text-sm
-    px-5 py-2.5 rounded-full
-    transition min-w-[120px] text-center`;
+  text-white font-medium text-sm
+    px-4 py-2.5 rounded-full
+  transition text-center`;
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-3">
@@ -42,8 +42,8 @@ export default function ComentarioInput({
       />
       {error && <p className="text-red-500 text-sm">{error}</p>}
 
-      <div className="flex justify-between items-center mt-2">
-        <div className="flex gap-4">
+      <div className="flex flex-wrap justify-center sm:justify-between items-center gap-2 mt-2">
+        <div className="flex gap-2">
           <button
             type="submit"
             className={`${baseBtn}
@@ -66,12 +66,13 @@ export default function ComentarioInput({
             Cancelar
           </button>
         </div>
-        <div>
+        <div className="ml-10">
           {onDelete && (
             <button
               type="button"
               onClick={onDelete}
               className={`${baseBtn}
+              text-sm px-4 py-2 sm:text-sm sm:px-4 sm:py-2
                 bg-gradient-to-r from-red-400 via-red-500
                  to-red-600 hover:bg-gradient-to-br focus:ring-4 
                  focus:outline-none focus:ring-red-300 dark:focus:ring-red-800
