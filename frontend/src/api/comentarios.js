650edbfedf95232c37a86e2511806d9ba2920ecf
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-07-06 20:16:58 +0200

    Refactor de Ver comentarios para usar los enlaces previous y next que proporciona la paginación de Django

--- frontend/src/api/comentarios.js
+++ frontend/src/api/comentarios.js
@@ -1,6 +1,5 @@
 import axiosAuthenticate from "../utils/axiosAuthenticate";
-import axiosPublic from '../utils/axiosPublic';
-
+import axiosPublic from "../utils/axiosPublic";
 
 export const crearComentario = async (id_asignatura, texto) => {
   const response = await axiosAuthenticate.post(
@@ -8,29 +7,39 @@ export const crearComentario = async (id_asignatura, texto) => {
     { texto }
   );
   return response.data;
-}
+};
 
-export const verComentarios = async (id_asignatura, pagina=1) => {
-  const response = await axiosPublic.get(`/comentarios/${id_asignatura}/?page=${pagina}`);
-  return response.data;
-}
+export const verComentarios = async (id_asignatura, url = null) => {
+  const baseUrl = `/comentarios/${id_asignatura}/`;
+
+  const response = await axiosPublic.get(url || baseUrl);
+
+  return {
+    results: response.data.results,
+    next: response.data.next,
+    previous: response.data.previous,
+    count: response.data.count,
+  };
+};
 
 export const editarComentario = async (id_comentario, texto) => {
-    const response = await axiosAuthenticate.put(
-      `/comentarios/editar/${id_comentario}/`,
-        { texto }
-    );
-    return response.data;
-}
+  const response = await axiosAuthenticate.put(
+    `/comentarios/editar/${id_comentario}/`,
+    { texto }
+  );
+  return response.data;
+};
 
 export const eliminarComentario = async (id_comentario) => {
-    const response = await axiosAuthenticate.delete(
-      `/comentarios/eliminar/${id_comentario}/`
-    );
-    return response.data;
-}
+  const response = await axiosAuthenticate.delete(
+    `/comentarios/eliminar/${id_comentario}/`
+  );
+  return response.data;
+};
 
 export const verComentario = async (id_comentario) => {
-    const response = await axiosAuthenticate.get(`/comentarios/ver/${id_comentario}/`);
-    return response.data;
-}
+  const response = await axiosAuthenticate.get(
+    `/comentarios/ver/${id_comentario}/`
+  );
+  return response.data;
+};
