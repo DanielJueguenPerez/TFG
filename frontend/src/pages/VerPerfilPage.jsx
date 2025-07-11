c941aaf5c0c341922fbfb32b8523c868fca67c05
Daniel Jueguen Pérez <d.jueguen@udc.es>
2025-07-11 20:37:19 +0200

    Modificado VerPerfilPage para cumplir con estandares de accesibilidad EAA

--- frontend/src/pages/VerPerfilPage.jsx
+++ frontend/src/pages/VerPerfilPage.jsx
@@ -61,7 +61,7 @@ export default function VerPerfilPage() {
               text-white
               bg-gradient-to-r from-purple-500 to-pink-500
               hover:from-pink-500 hover:to-purple-500
-              focus:outline-none focus:ring-4 focus:ring-purple-200
+              focus:outline-none focus:ring-4 focus:ring-purple-800
               font-medium rounded-full text-sm
               px-5 py-2.5
               transition
@@ -77,7 +77,7 @@ export default function VerPerfilPage() {
               text-white
               bg-gradient-to-r from-purple-500 to-pink-500
               hover:from-pink-500 hover:to-purple-500
-              focus:outline-none focus:ring-4 focus:ring-purple-200
+              focus:outline-none focus:ring-4 focus:ring-purple-800
               font-medium rounded-full text-sm
               px-5 py-2.5
               transition
